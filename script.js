ff53012fec4df116bf97070a79f87290415c78ab
criftore <taufikboy392@gmail.com>
2026-04-05 22:07:35 +0700

    put an operator function for recalling basic math function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // math basic function
-
+let fValue = "";
+let sValue = "";
 // add
 
 const add = function (a, b) {
@@ -18,4 +19,20 @@ const multiply = function (a, b) {
 // divide
 const divide = function (a, b) {
   return a / b;
+};
+
+const operation = function (operator, fValue, sValue) {
+  if (fValue === null && fValue === NaN) {
+    alert("Masukkan nilai pertama!!!");
+  } else {
+    if (operator === "+") {
+      return add(fValue, sValue);
+    } else if (operator === "-") {
+      return subtract(fValue, sValue);
+    } else if (operator === "*") {
+      return multiply(fValue, sValue);
+    } else if (operator === "/") {
+      return divide(fValue, sValue);
+    }
+  }
 };
