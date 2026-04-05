67cacf21d2c97882806d43782f9f55ea18e9fd64
criftore <taufikboy392@gmail.com>
2026-04-05 08:59:35 +0700

    Implement the basic match operator on the javascript

---
+++ script.js
@@ -0,0 +1,21 @@
+// math basic function
+
+// add
+
+const add = function (a, b) {
+  return a + b;
+};
+// subtract
+const subtract = function (a, b) {
+  return a - b;
+};
+
+// multiply
+const multiply = function (a, b) {
+  return a * b;
+};
+
+// divide
+const divide = function (a, b) {
+  return a / b;
+};
