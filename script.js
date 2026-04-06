d45a12563524ee5b82239be1d051e5ca5ac97c10
criftore <taufikboy392@gmail.com>
2026-04-06 23:44:14 +0700

    make Operation work with button and give the final value

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
 // math basic function
 let fValue = "";
 let sValue = "";
+let cOp = "";
 // add
 
 let vBtn = document.querySelectorAll(".vBtn");
 let dCal = document.querySelector(".formDisplay");
+let opBtn = document.querySelectorAll(".opBtn");
 
-vBtn.forEach((value) => {
-  value.addEventListener("click", function () {
-    if (dCal.value === "...") {
-      dCal.value = "";
-    }
-    const idBtn = this.id;
-    const dBtn = this.innerText;
-    dCal.value += dBtn;
-
-    console.log(`ID : ${idBtn} Value :${dBtn}`);
-  });
-});
+let cBtn = document.querySelector(".cBtn");
+let opValue = document.querySelector(".opValue");
 
 const add = function (a, b) {
   return a + b;
@@ -45,10 +37,56 @@ const operation = function (operator, fValue, sValue) {
       return add(fValue, sValue);
     } else if (operator === "-") {
       return subtract(fValue, sValue);
-    } else if (operator === "*") {
+    } else if (operator === "X") {
       return multiply(fValue, sValue);
     } else if (operator === "/") {
       return divide(fValue, sValue);
     }
   }
 };
+
+vBtn.forEach((value) => {
+  value.addEventListener("click", function () {
+    if (dCal.value === "...") {
+      dCal.value = "";
+    }
+    const idBtn = this.id;
+    const dBtn = this.innerText;
+
+    dCal.value += dBtn;
+
+    console.log(`ID : ${idBtn} Value :${dBtn}`);
+  });
+});
+
+opBtn.forEach((operate) => {
+  operate.addEventListener("click", function () {
+    if (dCal.value !== "") {
+      fValue = Number(dCal.value);
+    }
+    cOp = this.innerText;
+
+    dCal.value += "\n" + cOp + " ";
+
+    console.log(`operasi : ${cOp} fValue : ${fValue}`);
+  });
+});
+
+opValue.addEventListener("click", function () {
+  let contentArea = dCal.value.split("\n");
+  let sValue = Number(contentArea[1].replace(/[^0-9.]/g, ""));
+
+  let final = operation(cOp, fValue, sValue);
+
+  dCal.value = `${fValue}${cOp}${sValue} \n ${final}`;
+  console.log(
+    `fValue :${fValue} Operation :${cOp} sValue : ${sValue} hasil :${final}`,
+  );
+});
+
+cBtn.addEventListener("click", function () {
+  dCal.value = "";
+  sValue = "";
+  cOp = "";
+  fValue = "";
+});
