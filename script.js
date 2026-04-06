27757943b7661a4c68a7d1dc88043e4e6ee4f499
criftore <taufikboy392@gmail.com>
2026-04-07 05:35:18 +0700

    make chaining math working, and fix issues with opBtn

--- script.js
+++ script.js
@@ -1,38 +1,55 @@
-// math basic function
+// Variable in Calculator
 let fValue = "";
 let sValue = "";
 let cOp = "";
-let final = "";
-// add
+let result = "";
 
-let dCal = document.querySelector(".recentDisplay");
+//Calculator Display
+
+let nCal = document.querySelector(".nowDisplay");
+let rCal = document.querySelector(".recentDisplay");
 let hCal = document.querySelector(".historyDisplay");
+
+//Calculator Button
 let vBtn = document.querySelectorAll(".vBtn");
 let opBtn = document.querySelectorAll(".opBtn");
+let rBtn = document.querySelector(".resultBtn");
+let dBtn = document.querySelector(".deleteBtn");
+let cBtn = document.querySelector(".clearBtn");
 
-let cBtn = document.querySelector(".cBtn");
-let opValue = document.querySelector(".opValue");
+//Basic Math Function
 
 const add = function (a, b) {
   return a + b;
 };
-// subtract
 const subtract = function (a, b) {
   return a - b;
 };
-
-// multiply
 const multiply = function (a, b) {
   return a * b;
 };
-
-// divide
 const divide = function (a, b) {
   return a / b;
 };
 
+//Insert vBtn on nCal
+vBtn.forEach((value) => {
+  value.addEventListener("click", function () {
+    if (nCal.innerText === "...") {
+      nCal.innerText = "";
+    }
+    const idBtn = this.id;
+    const nBtn = this.innerText;
+
+    nCal.innerText += nBtn;
+
+    console.log(`Value :${nBtn}`);
+  });
+});
+
+//Implement opBtn on Basic Math Function
 const operation = function (operator, fValue, sValue) {
-  if (fValue === null && fValue === NaN) {
+  if (fValue === null || isNaN(fValue)) {
     alert("Masukkan nilai pertama!!!");
   } else {
     if (operator === "+") {
@@ -47,52 +64,56 @@ const operation = function (operator, fValue, sValue) {
   }
 };
 
-vBtn.forEach((value) => {
-  value.addEventListener("click", function () {
-    if (dCal.innerText === "...") {
-      dCal.innerText = "";
+//Logic on opBtn
+opBtn.forEach((event) => {
+  event.addEventListener("click", function () {
+    if (fValue !== "" && nCal.innerText !== "" && cOp !== "") {
+      sValue = Number(nCal.innerText);
+      result = operation(cOp, fValue, sValue);
+      fValue = result;
+      cOp = this.innerText;
+      hCal.innerText = result;
+      rCal.innerText = `${fValue} ${cOp} ${sValue}`;
+      nCal.innerText = "";
+    } else if (nCal.innerText !== "") {
+      fValue = Number(nCal.innerText);
+      cOp = this.innerText;
+      rCal.innerText = `${fValue} ${cOp} ...`;
+      nCal.innerText = "";
+    } else if (fValue !== "") {
+      cOp = this.innerText;
+      rCal.innerText = `${fValue} ${cOp} ...`;
+      nCal.innerText = "";
     }
-    const idBtn = this.id;
-    const dBtn = this.innerText;
-
-    dCal.innerText += dBtn;
-
-    console.log(`ID : ${idBtn} Value :${dBtn}`);
   });
 });
 
-opBtn.forEach((operate) => {
-  operate.addEventListener("click", function () {
-    if (dCal.innerText !== "") {
-      fValue = Number(dCal.innerText);
-    }
-    cOp = this.innerText;
-    hCal.innerText = fValue;
-    dCal.innerText = cOp + " ";
-
-    console.log(`operasi : ${cOp} fValue : ${fValue}`);
-  });
-});
-
-opValue.addEventListener("click", function () {
-  let sValue = Number(dCal.innerText.replace(/[^0-9.]/g, ""));
-
-  // let contentArea = dCal.value.split("\n");
-  // let sValue = Number(contentArea[1].replace(/[^0-9.]/g, ""));
-
-  final = operation(cOp, fValue, sValue);
-
-  hCal.innerText = fValue + cOp + sValue;
-
-  dCal.innerText = final;
-
-  console.log(`${fValue} ${cOp} ${sValue}`);
-});
-
-cBtn.addEventListener("click", function () {
-  dCal.innerText = "";
+//Logic on ResultBtn
+rBtn.addEventListener("click", function () {
+  let sValue = Number(nCal.innerText);
+  result = operation(cOp, fValue, sValue);
+  rCal.innerText = `${fValue} ${cOp} ${sValue}`;
+  nCal.innerText = result;
+  fValue = result;
   sValue = "";
   cOp = "";
+  console.log(`${fValue} ${cOp} ${sValue} = ${result}`);
+});
+
+//Logic on DeteleBtn
+dBtn.addEventListener("click", function () {
+  nCal.innerText = nCal.innerText.slice(0, -1);
+  if (nCal.innerText === "") {
+    nCal.innerText = "...";
+  }
+});
+
+//Logic on ClearBtn
+cBtn.addEventListener("click", function () {
+  nCal.innerText = "";
+  rCal.innerText = "";
+  hCal.innerText = "";
   fValue = "";
-  hCal.innerText = final;
+  sValue = "";
+  cOp = "";
 });
